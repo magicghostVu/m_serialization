a6cf6df957f8897ddbdd59c959646625a386a7de
magicghostvu <magicghost.vu@gmail.com>
2023-11-03 23:52:51 +0700

    #refactor map equals

--- m_serialization_annotation/src/main/kotlin/m_serialization/utils/MEqualsUtils.kt
+++ m_serialization_annotation/src/main/kotlin/m_serialization/utils/MEqualsUtils.kt
@@ -1,11 +1,15 @@
 package m_serialization.utils
 
 object MEqualsUtils {
-    fun <T> equalsTwoLists(listA: List<T>, listB: List<T>, compareElement: (a: T, b: T) -> Boolean): Boolean {
-        return if (listA.size != listB.size) false
+    fun <T> equalsTwoCollections(
+        collectionA: Collection<T>,
+        collectionB: Collection<T>,
+        compareElement: (a: T, b: T) -> Boolean
+    ): Boolean {
+        return if (collectionA.size != collectionB.size) false
         else {
-            val i1 = listA.listIterator()
-            val i2 = listB.listIterator()
+            val i1 = collectionA.iterator()
+            val i2 = collectionB.iterator()
             var hadDiff = false
             while (i1.hasNext()) {
                 val e1 = i1.next()
@@ -21,20 +25,10 @@ object MEqualsUtils {
 
     // vì hiện tại chỉ hỗ trợ key là các primitive nên là đối với key sẽ dùng equals mặc định để so sánh
     fun <K, V> equalsToMap(mapA: Map<K, V>, mapB: Map<K, V>, compareValue: (v1: V, v2: V) -> Boolean): Boolean {
-        return if (mapA.size != mapB.size) false
-        else {
-            val ia = mapA.iterator()
-            val ib = mapB.iterator()
-            var hadDiff = false
-            while (ia.hasNext()) {
-                val (ka, va) = ia.next()
-                val (kb, vb) = ib.next()
-                if (ka != kb || !compareValue(va, vb)) {
-                    hadDiff = true
-                    break
-                }
-            }
-            !hadDiff
+        return equalsTwoCollections(mapA.entries, mapB.entries) { ea, eb ->
+            val (ka, va) = ea
+            val (kb, vb) = eb
+            ka == kb && compareValue(va, vb)
         }
     }
 }
